17e6d4e321e9f617840802322daf5bcd2a280b20
ahob85 <aaron@ahob85.com>
2018-10-11 00:45:04 -0700

    Update methods.js

---
+++ methods.js
@@ -0,0 +1,42 @@
+// **** Example: sayHello() ****
+// Write a function expression that prints "Hi " along with whatever string is
+// passed in as input.
+let sayHello = function(message) {
+  console.log(message);
+}
+
+// Test your function below
+sayHello("Hello World!");
+
+
+// **** Problem 1: getQuotient() ****
+// Write a function expression that returns the quotient of the first argument
+// divided by the second.
+
+
+// Test your function below
+
+
+// **** Problem 2: sumDouble() ****
+// Write a function expression that, when given two number values, return their
+// sum. Unless the two values are the same, then return double their sum.
+
+// Test your function below
+
+
+// **** Problem 3: makes10() ****
+// Write a function expression that, when govem 2 numbers, a and b, returns true
+// if one if them is 10 or if their sum is 10.
+
+
+// Test your function below
+
+
+// **** Problem 4: Object Methods ****
+// Write code that represents any object you like. Your object should have at
+// least four properties and four methods. At least two of your methods should
+// utilize the "this" keyword to refer to properties, or call other methods,
+// owned by the object.
+
+
+// Test your object's methods below.
