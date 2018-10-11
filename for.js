3bb3e99446a54727c1e57378a7e270bb0fe3efa2
ahob85 <aaron@ahob85.com>
2018-10-11 01:44:48 -0700

    update

---
+++ for.js
@@ -0,0 +1,40 @@
+const READLINE = require("readline-sync");
+
+/**** Problem 1 ****
+Given a string of any length, return true if it contains at least one space.
+
+Examples:
+containsSpace("star wars") → true
+containsSpace("     ") → true
+containsSpace("") → false
+*/
+
+function containsSpace(str) {
+
+}
+
+/**** Problem 2 ****
+Given a string of any length, return the total number of times "a" or "A" appears.
+
+Examples:
+countAs("happy") → 1
+countAs("lady gaga") → 3
+countAs("") → 0
+*/
+
+function countAs(str) {
+
+}
+
+/**** Problem 3 ****
+Given a string of any length, return a new string that is a copy of that string in reverse.
+
+Examples:
+reverseString("banana") → "ananab"
+reverseString("Was it a car or a cat I saw?") → "?was I tac a ro rac a ti saW"
+reverseString("doorag") → "garood"
+*/
+
+function reverseString(str) {
+
+}
